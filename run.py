53bfb95a8d06321dd52e155591b0036ac27dda98
SarahKamunya <sarahkamunya254@gmail.com>
2022-04-26 08:49:58 +0300

    add create_user in run.py

--- run.py
+++ run.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3.6
 from password import User, Credentials
+
+def create_user(username, password):
+    '''
+    Function to create a new user with a username and password
+    '''
+    new_user = User(username, password)
+    return new_user
+    
+    
